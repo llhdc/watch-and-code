1ab4ae728d57e9621716616e0b5de1560cd37613
Leonard <leonard.horne@gmail.com>
2017-10-19 16:16:40 -0400

    declare todos array. add displayTodos to function. add deleteTodo func.

--- todoapp.js
+++ todoapp.js
@@ -7,7 +7,7 @@
 
 // requirements
 // - it should have a place to store todos
-todos = ['item 1', 'item 2', 'item 3', 'item 4']
+let todos = ['item 1', 'item 2', 'item 3', 'item 4']
 
 // - it should have a way to display todos
 console.log('My todos:', todos);
@@ -30,10 +30,16 @@ function displayTodos(todos) {
 }
 // - it should have a way to add todos
 function addTodo(todo) {
-  todos.push(todo)
+  todos.push(todo);
+  displayTodos();
 }
 // - it should have a way to change a todo
 function changeTodo(position, newTodo) {
-  todos[position] = newTodo
+  todos[position] = newTodo;
+  displayTodos();
 }
 // - it should have a way to delete todos
+function deleteTodo(position) {
+  todos.splice(position, 1);
+  displayTodos();
+}
