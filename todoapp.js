8e8848d35b1c560c669b1dad86a4fc1ffc199e48
Leonard <leonard.horne@gmail.com>
2017-10-20 17:20:45 -0400

    add addTodo method to todoList object

--- todoapp.js
+++ todoapp.js
@@ -71,5 +71,9 @@ var todoList = {
   todos: ['item 1', 'item 2', 'item 3'],
   displayTodos: function() {
     console.log('My todos:', this.todos);
+  },
+  addTodo: function() {
+    this.todos.push(todo);
+    this.displayTodos();
   }
 };
