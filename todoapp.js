5b6f2719f157b6b7abec76ce9e8f1c1a455c7069
Leonard <leonard.horne@gmail.com>
2017-10-20 16:59:57 -0400

    create todoList object. add displayTodos method.

--- todoapp.js
+++ todoapp.js
@@ -43,3 +43,33 @@ function deleteTodo(position) {
   todos.splice(position, 1);
   displayTodos();
 }
+
+v3
+objects
+
+let myMac = {
+  operatingSystem: 'mac',
+  screenSize: '15 inches',
+  purchaseYear: 2011
+}
+
+let leonard = {
+  name: 'Leonard',
+  sayName: function() {       // functions on objects are methods;
+    console.log(this.name);
+  }
+}
+
+//requirements
+// it should store the todos array on an object
+// it should have a displayTodos method
+// it should have an addTodo method
+// it should have a changeTodo method
+// it should have a deleteTodo method
+
+var todoList = {
+  todos: ['item 1', 'item 2', 'item 3'],
+  displayTodos: function() {
+    console.log('My todos:', this.todos);
+  }
+};
