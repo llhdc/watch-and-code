f037d6b6320b36b103a4243ab14234b550216a0e
Leonard <leonard.horne@gmail.com>
2017-10-21 11:42:14 -0400

    include logic in displayTodos method.

--- todoapp.js
+++ todoapp.js
@@ -115,3 +115,53 @@ let todoList = {
     this.displayTodos();
   }
 };
+
+// v5
+// loops
+
+i = 0 // initialization expression
+say "hey" if i < 3 // conditional expression
+increase i by 1 // final-expression
+
+for (let i = 0; i < 3; i++) {
+  console.log("hey");
+}
+
+// requirements
+// .displayTodos should show .todoText
+// .displayTodos should tell you if .todos is empty
+// .displayTodos should show .completed
+
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My todos:');
+      for (let i = 0; i < this.todos.length; i++) {
+        console.log(this.todos[i].todoText)
+      }
+    }
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed
+    this.displayTodos();
+  }
+};
