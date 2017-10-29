f9d011d18c429f56ad327dc6a36878975d06ab98
Leonard <leonard.horne@gmail.com>
2017-10-29 14:12:02 -0400

    v10 requirements.

---
+++ todoapp-v10.js
@@ -0,0 +1,9 @@
+/*
+v10
+requirements
+- there should be a way to create delete buttons
+- there should be a delete button for each todo
+- each li should have an id that has the todo position
+- delete buttons should have access to the todo li
+- clicking delete should update todoList.todos and the DOM
+*/
