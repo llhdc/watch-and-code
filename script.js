b6d14ad92922dec6d5b86a83aae776a89b6c08b0
Leonard <leonard.horne@gmail.com>
2017-10-29 21:26:00 -0400

    replace for loops with forEach for readibility.

--- script.js
+++ script.js
@@ -27,23 +27,24 @@ let todoList = {
     let totalTodos = this.todos.length;
     let completedTodos = 0;
 
-    // Count completed todos.
-    for (let i = 0; i < totalTodos; i++) {
-      if (this.todos[i].completed === true) {
+    // // Count completed todos.
+    this.todos.forEach(function(todo){
+      if (todo.completed === true) {
         completedTodos++;
       }
-    }
+    });
 
-    // Case 1: if everything's true, make everything false.
+    // Case 1: If everything’s true, make everything false.
     if (completedTodos === totalTodos) {
-      for (let i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
-      }
+      this.todos.forEach(function(todo){
+        todo.completed = false;
+      });
+
     // Case 2: Otherwise, make everything true.
     } else {
-      for (let i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }
+      this.todos.forEach(function(todo){
+        todo.completed = true;
+      });
     }
   }
 };
